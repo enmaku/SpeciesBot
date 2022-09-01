83db15c7ce742b9417bd769438ac7caa040c53d5
enmaku <enmaku@gmail.com>
2022-08-31 20:58:35 -0700

    loading the rest of the things from files, commenting out subreddit-specific rules for now

--- speciesbot.py
+++ speciesbot.py
@@ -9,18 +9,25 @@ from datetime import datetime
 
 startTime = datetime.utcnow()
 
-list_of_names = ["enmaku", "modestmenagerie"]
-subreddits = ["speciesbot_testing"]
-sig = "*I am a bot created to help with animal identification and natural history education."
+list_of_names = []
+with open("reliable.txt", "r") as f:
+    list_of_names = f.read().splitlines()
+
+subreddits = []
+with open("subreddits.txt", "r") as f:
+    subreddits = f.read().splitlines()
+
+with open("signature.md", "r") as f:
+    sig = f.read()
 
 commands = []
-for filename in os.listdir('commands'):
-	with open(os.path.join('commands', filename), "r") as f:
+for filename in os.listdir("commands"):
+	with open(os.path.join("commands", filename), "r") as f:
 		commandtext = f.read()
-		commands.append({'command': Path(filename).stem, 'text': commandtext})
+		commands.append({"command": Path(filename).stem, "text": commandtext})
 
 specieslist = []
-for filename in os.listdir('species'):
+for filename in os.listdir("species"):
     specieslist.append(Path(filename).stem)
 
 def bot_login():
@@ -49,9 +56,9 @@ def checkComment(comment):
 				print("Replied to comment " + comment.id)
 
 	for command in commands:
-		if "!" + command['command'] in comment.body:
-			print("!" + command['command'] + " found in comment " + comment.id)
-			bldr.append(command['text']) 
+		if "!" + command["command"] in comment.body:
+			print("!" + command["command"] + " found in comment " + comment.id)
+			bldr.append(command["text"]) 
 	if len(bldr): 
 		bldr.append(sig) 
 		comment.reply(body="\n\n--------------------------------------------------------\n\n".join(bldr))
@@ -73,38 +80,38 @@ def run_bot(r):
 		checkComment(comment)
 
 	# Subreddit-specific rules
-	for submission in r.subreddit('whatsthissnake').new(limit=10):
-		if submission.saved or submission.author == r.user.me() or datetime.utcfromtimestamp(submission.created_utc) < startTime:
-			break
+	# for submission in r.subreddit("whatsthissnake").new(limit=10):
+	# 	if submission.saved or submission.author == r.user.me() or datetime.utcfromtimestamp(submission.created_utc) < startTime:
+	# 		break
 
-		if len(re.findall('\[.+\]', submission.title)) == 0:
-			submission.reply("It looks like you didn't provide a rough geographic location [in square brackets] in your title. "
-			                 "Some species are best distinguishable from each other by geographic range, and not all "
-			                 "species live all places. Providing a location allows for a quicker, more accurate ID."
-			                 + "\n\n" + "If you provided a location but forgot the correct brackets, ignore this message "
-			                            "until your next submission. Thanks!" + "\n\n" + sig)
-			print("Replied to submission " + submission.id)
+	# 	if len(re.findall("\[.+\]", submission.title)) == 0:
+	# 		submission.reply("It looks like you didn't provide a rough geographic location [in square brackets] in your title. "
+	# 		                 "Some species are best distinguishable from each other by geographic range, and not all "
+	# 		                 "species live all places. Providing a location allows for a quicker, more accurate ID."
+	# 		                 + "\n\n" + "If you provided a location but forgot the correct brackets, ignore this message "
+	# 		                            "until your next submission. Thanks!" + "\n\n" + sig)
+	# 		print("Replied to submission " + submission.id)
 
-		if submission.link_flair_text == "Dead, Injured or Roadkilled Snake":
-			submission.reply("This automatic message accompanies any image of a dead, injured or roadkilled snake: " + "\n\n" +
-			                 "Please don't kill snakes - they are a natural part of the ecosystem and [even species that "
-			                 "use venom for prey acquisition and defense are beneficial to humans]"
-			                 "(https://web.archive.org/web/20180802190346/https://umdrightnow.umd.edu/news/timber-rattlesnakes-vs-lyme-disease). One cannot expect "
-			                 "outside to be sterile - if you see a snake you're in or around their preferred habitat. "
-			                 "Most snakes are valued and as such are protected from collection, killing or harassment "
-			                 "as non-game animals at the state level.\n\n[Neighborhood dogs]"
-			                 "(http://livingalongsidewildlife.com/?p=3141) "
-			                 "are more likely to harm people. Professional snake relocation services are often free or "
-			                 "inexpensive, but snakes often die trying to return to their original home range, so it is "
-			                 "usually best to enjoy them like you would songbirds or any of the other amazing wildlife "
-			                 "native to your area. Commercial snake repellents are not effective - to discourage snakes, "
-			                 "eliminate sources of food and cover; clear debris, stacked wood and eliminate rodent "
-			                 "populations. Seal up cracks in and around the foundation/base of your home." + "\n\n" + sig)
-			print("Replied to Dead Snake flair - " + submission.id)
+	# 	if submission.link_flair_text == "Dead, Injured or Roadkilled Snake":
+	# 		submission.reply("This automatic message accompanies any image of a dead, injured or roadkilled snake: " + "\n\n" +
+	# 		                 "Please don't kill snakes - they are a natural part of the ecosystem and [even species that "
+	# 		                 "use venom for prey acquisition and defense are beneficial to humans]"
+	# 		                 "(https://web.archive.org/web/20180802190346/https://umdrightnow.umd.edu/news/timber-rattlesnakes-vs-lyme-disease). One cannot expect "
+	# 		                 "outside to be sterile - if you see a snake you're in or around their preferred habitat. "
+	# 		                 "Most snakes are valued and as such are protected from collection, killing or harassment "
+	# 		                 "as non-game animals at the state level.\n\n[Neighborhood dogs]"
+	# 		                 "(http://livingalongsidewildlife.com/?p=3141) "
+	# 		                 "are more likely to harm people. Professional snake relocation services are often free or "
+	# 		                 "inexpensive, but snakes often die trying to return to their original home range, so it is "
+	# 		                 "usually best to enjoy them like you would songbirds or any of the other amazing wildlife "
+	# 		                 "native to your area. Commercial snake repellents are not effective - to discourage snakes, "
+	# 		                 "eliminate sources of food and cover; clear debris, stacked wood and eliminate rodent "
+	# 		                 "populations. Seal up cracks in and around the foundation/base of your home." + "\n\n" + sig)
+	# 		print("Replied to Dead Snake flair - " + submission.id)
 
-		submission.save()
+	# 	submission.save()
 		
-	for submission in r.subreddit('Herpetology').new(limit=10): ##change to test 'whatsthissnake'
+	for submission in r.subreddit("Herpetology").new(limit=10): ##change to test "whatsthissnake"
 		if submission.saved or submission.author == r.user.me() or datetime.utcfromtimestamp(submission.created_utc) < startTime:
 			break
 
