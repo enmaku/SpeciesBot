1174dc8448c85f3872aff8e514a22ac413f5040c
enmaku <enmaku@gmail.com>
2022-09-23 13:01:05 -0700

    something about threading was hanging after successful responses

--- speciesbot.py
+++ speciesbot.py
@@ -177,19 +177,13 @@ def subreddit_post_rules(r):
     
     
 def run_bot(r):	
-	threads = []
 	if config.reliable_responders:
-		threads.append(threading.Thread(target=reliable_responders, args=(r,)))
+		reliable_responders(r)
 	if config.subreddit_comments:
-		threads.append(threading.Thread(target=subreddit_comments, args=(r,)))
+		subreddit_comments(r)
 	if config.subreddit_rules:
-		threads.append(threading.Thread(target=subreddit_post_rules, args=(r,)))
+		subreddit_post_rules(r)
   
-	for thread in threads:
-		thread.start()
-	for thread in threads:
-		thread.join()
-
 
 r = bot_login()
 
